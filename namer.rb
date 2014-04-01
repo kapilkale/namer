e048b14163547390f8ce1feb641872796fba01bf
Kapil Kale <kapil.kale.07@gmail.com>
2014-03-31 22:04:34 -0700

    update

--- namer.rb
+++ namer.rb
@@ -4,20 +4,42 @@ threads = []
 
 f = File.open("names.txt")
 body = f.read
-body.downcase.split(" ").uniq.map{|l| l.gsub(/[^0-9a-z ]/i, '')}.each do |b|
+
+# Filter words to check. Modify character length as you please.
+words_to_check = body.downcase.split(" ").uniq
+    .map{|l| l.gsub(/[^0-9a-z ]/i, '')}
+    .select{|l| l.length < 5}
+    
+
+words_to_check.each_with_index do |b, index|
   t = Thread.new do
     begin
       c = Whois::Client.new
-      name = "#{b}.com"
+
+      # Run checks with specific characters attached.
+      prepend = ""
+      append = ""
+      name = "#{prepend}#{b}#{append}.com"
+
+      # Actually do lookup.
       r = c.lookup(name)
       puts "#{name} - available\n" if r.available?
     rescue Exception => e
-      puts "#{name} - exception\n"
+      #puts "#{name} - #{e}\n" unless e.is_a? Errno::ECONNRESET
     end
   end
   threads << t
+
+  # Never more than 100 threads at once.
+  if index > 0 && index % 100 == 0
+    threads.each do |t|
+      t.join
+    end
+    threads = []
+  end
 end
 
+# Close out the threads.
 threads.each do |t|
   t.join
 end
